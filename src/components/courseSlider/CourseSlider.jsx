5b30bf8d722baf8c5f396b76252e0bca73464c2d
prasojopxl <prasojodesigner@gmail.com>
2021-02-17 22:13:32 +0700

    add slick

--- src/components/courseSlider/CourseSlider.jsx
+++ src/components/courseSlider/CourseSlider.jsx
@@ -1,33 +1,106 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Slider from "react-slick";
+import imageCover from "../../assets/images/cover-dev.jpg";
+import "./style.scss";
 
 export const CourseSlider = () => {
     var settings = {
-        dots: true,
         infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        arrows: true,
+		// prevArrow: 'aa',
+		// nextArrow: '87',        
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                  arrows: false,
+                  centerMode: true,
+                  centerPadding: '40px',
+                  slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                  arrows: false,
+                  centerMode: true,
+                  centerPadding: '40px',
+                  slidesToShow: 2
+                }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+              }
+            }
+          ]        
     };
 
+    const [product, setProduct] = useState({
+        "products": [
+            {
+                "cover":imageCover,
+                "title": "Frontend Dev with Vue",
+                "level":"Basic",
+                "price":"Rp. 799.000"
+            },
+            {
+                "cover":imageCover,
+                "title": "NodeJS with Express",
+                "level":"Basic",
+                "price":"Rp. 599.000"
+            },
+            {
+                "cover":imageCover,
+                "title": "SEO",
+                "level":"Medium",
+                "price":"Rp. 500.000"
+            },
+            {
+                "cover":imageCover,
+                "title": "Rest API",
+                "level":"Medium",
+                "price":"Rp. 510.000"
+            },
+            {
+                "cover":imageCover,
+                "title": "Fundamental React Native",
+                "level":"Basic",
+                "price":"Rp. 490.000"
+            },
+            {
+                "cover":imageCover,
+                "title": "Golang",
+                "level":"Basic",
+                "price":"Rp. 899.000"
+            },            
+        ]
+    })
+
     return (
-        <Slider {...settings}>
-            <div>
-                <h3>1</h3>
-            </div>
-            <div>
-                <h3>2</h3>
-            </div>
-            <div>
-                <h3>3</h3>
-            </div>
-            <div>
-                <h3>4</h3>
-            </div>
-            <div>
-                <h3>5</h3>
-            </div>
-        </Slider>
+        <div className="wrp-sliderproduct">            
+            <Slider {...settings}>
+                {   
+                    product.products.map((item,i)=> {
+                        return (
+                            <div key={i} className="itemslider">
+                                <img src={item.cover} alt="sdsa"/>
+                                <h3>{item.title}</h3>
+                                <h5>{item.level}</h5>
+                                <div className="price">{item.price}</div>
+                            </div>
+                        )
+                    })                
+                
+                }
+            </Slider>
+        </div>        
     )
 }
 
