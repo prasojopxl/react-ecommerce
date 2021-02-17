e52db1e8331d5e5e1b034272fc0f123657d389c7
prasojopxl <prasojodesigner@gmail.com>
2021-02-17 18:58:16 +0700

    add slick

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import {Baner} from "../../components/baner/Baner"
 import {Feature} from "../../components/feature/Feature";
 import {Benefit} from "../../components/bennefit/Benefit";
+import {CourseSlider} from "../../components/courseSlider/CourseSlider";
+
 export const Home = () => {
     return (
         <div>
             <Baner/>
             <Feature/>
             <Benefit/>
+            <CourseSlider/>
         </div>
     )
 }
