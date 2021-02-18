ab2c3119d76857e93a49f7f69a3c8d78947c0e88
prasojopxl <prasojodesigner@gmail.com>
2021-02-18 16:36:28 +0700

    add detail page

--- src/pages/kelas/KelasDetail.jsx
+++ src/pages/kelas/KelasDetail.jsx
@@ -7,7 +7,7 @@ export const KelasDetail = () => {
     return (
         <div className="container wrp-kelasdetail">
             <div style={{margin: "90px auto", textAlign:"center"}}>
-                Welcome Page 
+                Selamat Datang di Kelas
                 <h3 style={{textTransform:"uppercase"}}>{params.id}</h3>
             </div>
         </div>
