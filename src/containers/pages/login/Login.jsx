38ac4c90b6fd4d16bd06c0fd5a13d17441ffdb76
prasojopxl <prasojodesigner@gmail.com>
2021-03-09 13:44:49 +0700

    add jwt

--- src/containers/pages/login/Login.jsx
+++ src/containers/pages/login/Login.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
-import {FormInput} from "../../../components/formInput/FormInput";
+import React, {useState} from 'react'
+import axios from "axios";
+import {useHistory} from "react-router-dom";
 
 
 export const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword]= useState ("")
+    const history = useHistory();
+
+    const login = () => {
+        axios.post (`http://localhost:4000/login`, {
+            email,
+            password
+        })
+        .then((res)=> {
+            console.log("berhasil",res)
+            localStorage.setItem("JWT", res.data.accessToken)
+            history.push("/dashboard");
+        })
+        .catch ((err)=> {
+            console.log("gagal",err)
+        })
+    }
+
     return (
         <div className="formpage">
             <div className="wrp-form">
                 <h4>Login</h4>
                 <div className="boxlogin">
-                    <FormInput type="text" placeholder="Username"/>
-                    <FormInput type="password" placeholder="Password"/>
-                    <FormInput type="submit" value="Submit"/>
+                    <div className="item-input">
+                        <input type="text" value={email} name="email" id="" onChange={(e) => { setEmail(e.target.value)}} placeholder="Name"/>
+                    </div>
+                    <div className="item-input">
+                        <input type="password" value={password} name="password" onChange={(e)=> {setPassword(e.target.value)}} placeholder="Password"/>
+                    </div>
+                    <div className="item-input">
+                        <input type="button" onClick={()=> login()} value="Submit"/>
+                    </div>
+
                 </div>
             </div>
         </div>
--- src/containers/pages/register/Register.jsx
+++ src/containers/pages/register/Register.jsx
@@ -1,18 +1,49 @@
-import React from 'react'
-import {FormInput} from "../../../components/formInput/FormInput";
+import React, {useState} from 'react'
+import axios from "axios";
 
 
 export const Register = () => {
+    const [name, setName]= useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+
+    const register = () => {
+        axios.post(`http://localhost:4000/register`,  {
+            name,
+            email,
+            password,
+            phone
+        })
+        .then ((res) => {
+            console.log("berhasil",res)
+        })
+        .catch((err)=> {
+            console.log("gagal",err)
+        })
+    }
+
+
     return (
         <div className="formpage">
             <div className="wrp-form">
                 <h4>Register</h4>
                 <div className="boxlogin">
-                    <FormInput type="text" placeholder="Nama Lengkap"/>
-                    <FormInput type="text" placeholder="Email"/>
-                    <FormInput type="text" placeholder="Username"/>
-                    <FormInput type="password" placeholder="Password"/>
-                    <FormInput type="submit" value="Submit"/>
+                    <div className="item-input">
+                        <input type="text" value={name} name="name" id="" onChange={(e) => { setName(e.target.value)}} placeholder="Name"/>
+                    </div>
+                    <div className="item-input">
+                        <input type="password" value={password} name="password" onChange={(e)=> {setPassword(e.target.value)}} placeholder="Password"/>
+                    </div>
+                    <div className="item-input">
+                        <input type="text" value={email} name="email" onChange={(e)=> {setEmail(e.target.value)}} placeholder="Email"/>
+                    </div>
+                    <div className="item-input">
+                        <input type="text" value={phone} name="phone" onChange={(e)=> {setPhone(e.target.value)}} placeholder="Phone"/>
+                    </div>
+                    <div className="item-input">
+                        <input type="button" onClick={()=> register()} value="Submit"/>
+                    </div>
                 </div>
             </div>
         </div>
